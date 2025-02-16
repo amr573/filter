e1dac7b237bcafdfdbb5056a7b71772c27730f76
amr573 <amrooozakaria@gmail.com>
2025-02-16 21:42:37 +0300

    Add files via upload

--- mine.js
+++ mine.js
@@ -1,132 +1,29 @@
-let dad=document.querySelector(".dad");
-let items=[
-{
-    img:"download.jpg",
-    name:"T-shert",
-    text:"the best T-shert in the Egypt . it is made in egypt and it is made from the skin ",
-    button:"buy",
-}
-,
-{
-    img:"shoes.jpg",
-    name:"shoes",
-    text:"te best shoes in the Egypt. it is made in Egypt and is is made from skin ",
-    button:"buy",
-}
-,
-{
-    img:"bag.jpg",
-    name:"bag",
-    text:"te best bag in the Egypt. it is made in Egypt and is is made from skin ",
-    button:"buy",
-}
-,
-{
-    img:"bike.jpg",
-    name:"bike",
-    text:"te best bike in the Egypt. it is made in Egypt and is is made from Gold ",
-    button:"buy",
-}
-,
-{
-    img:"book.jpg",
-    name:"book",
-    text:"te best book in the Egypt. it is made in Egypt and is is made from skin ",
-    button:"buy",
-}
-,
-{
-    img:"phon.jpg",
-    name:"phon",
-    text:"te best phon in the Egypt. it is made in Egypt and is is made from gold ",
-    button:"buy",
-}
-,
-{
-    img:"tabal.jpg",
-    name:"tabal",
-    text:"te best tabal in the Egypt. it is made in Egypt and is is made from gold ",
-    button:"buy",
-}
-,
-{
-    img:"carsjpg.jpg",
-    name:"cars",
-    text:"te best cars in the Egypt. it is made in Egypt and is is made from gold ",
-    button:"buy",
-}
-,
-{
-    img:"glass.jpg",
-    name:"glasses",
-    text:"te best glasses in the Egypt. it is made in Egypt and is is made from gold ",
-    button:"buy",
-}
-,
-{
-    img:"cap.jpg",
-    name:"cap",
-    text:"te best cap in the Egypt. it is made in Egypt and is is made from skin ",
-    button:"buy",
-}
-,
-{
-    img:"coleor.jpg",
-    name:"color",
-    text:"te best color in the Egypt. it is made in Egypt and is is made from best wood ",
-    button:"buy",
-}
-,
+let but=document.querySelector(".but")
+let pra=document.querySelector(".pra")
+let pre=document.querySelector(".pre")
 
-]
-
-
-let bu=document.querySelector(".bu")
-let len=items.length;
-
-      
-       
-        
-       let inp=document.querySelector(".inp")
+function grade(){
+    let inp=document.querySelector(".inp").value
+    let d=(inp/360)*100
      
-
-      let fillterdItem = []
-      inp.addEventListener("change", () => {
-        fillterdItem = items.filter(el => el.name.includes(inp.value));
-        show()
-      })
-
-      function show(){
-        dad.innerHTML=` `
-  if(fillterdItem.length>0){
-  
-      fillterdItem.forEach((item)=>{
-           dad.innerHTML+=`
-          <div class="devo">
-          <img class="img"src=${item.img} >
-          <h2 class="name">${item.name}</h2>
-          
-          <p class="pra">${item.text}</p>
-          <button class="buy">${item.button}</button>
-          
-          </div>`
-          })
-  }else{
-      
-    items.forEach((item)=>{
-      
-          dad.innerHTML+=`
-        <div class="devo">
-        <img class="img"src=${item.img} >
-        <h2 class="name">${item.name}</h2>
+    if(d>=85 & d<=100){
+        pra.textContent="Excellent"
+    }else if(d>=75 & d<85){
+        pra.textContent="very good"
+    }else if(d>=65 & d<75){
+        pra.textContent="good"
+    } else if(d>=50 & d<65){
+        pra.textContent="acceptable"
+    } else if(d>=0 & d<50){
+        pra.textContent="bad!!"
         
-        <p class="pra">${item.text}</p>
-        <button class="buy">${item.button}</button>
-        
-        </div>`
-      
-      })
-  }
-  
-  }
-  show()
+    }else{
+        pra.textContent="not a thing"
+    }
+    pre.textContent=d
+}
+
+but.onclick=grade
+
+
+
